98ee6a60eba36b27dff675f7c35d69bbf13d6526
Nourin <fathimanourin363@gmail.com>
2025-06-12 09:53:09 +0530

    Initial commit

---
+++ operations.py
@@ -0,0 +1,24 @@
+#Arithmetic opertation
+a=10
+b=5
+print(a+b)
+print(a%b)
+print(a//b)
+print(a/b)
+#Comparison or Rational operators
+a=5
+b=5
+print(a==b)
+print(a>b)
+print(a<b)
+print(a>=b)
+print(a<=b)
+#Assignment operations
+a=20
+print(a=)
+print(a+=)
+print(a*=)
+print(a/=,a)
+print(a%=,a)
+print(a**=,a)
+print(a//=,a)
