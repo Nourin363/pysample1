b95480a6f427d78550c72e6776409c08f1a42825
Nourin <fathimanourin363@gmail.com>
2025-06-17 10:30:14 +0530

    Control Statement

---
+++ control.py
@@ -0,0 +1,56 @@
+#     #if statement
+# i = 10
+
+# if i>15:
+# 	print("10 is less than 15")
+# print("I am Not in if")
+
+
+
+     #if else statement
+# i=20
+# if i<15:
+#     print("i is smaller than 15")
+#     print("i'm in if block")
+# else :
+#     print("i is greater than 15")
+#     print("i'm in else Block")
+# print("i'm not in if and not in else Block")
+
+
+#nested if statement
+# i = 10
+# if i == 10:
+#     if i < 15:
+#         print("i is smaller than 15")
+#     if i < 12:
+#         print("i is smaller than 12 too")
+#     else:
+#         print("i is greater than 15")
+
+
+#if elif else ladder
+# i = 20
+# if i == 10:
+#     print("i is 10")
+# elif i == 15:
+#     print("i is 15")
+# elif i == 20:
+#     print("i is 20")
+# else:
+#     print("i is not present")
+
+i=90
+if i<=100 and i>=0:
+    if i>=90:
+        print("A Grade")
+    elif i>=80:
+        print("B Grade")
+    elif i>=70:
+        print("C Grade")
+    elif i>=60:
+        print("D Grade")
+    else:
+        print("Fail")
+else:
+    print("Invalid Mark")
