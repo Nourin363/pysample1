7cbf1415d3548e74bdf4c29f44200460b2645560
Nourin <fathimanourin363@gmail.com>
2025-06-20 06:50:58 +0530

    loops

---
+++ loop.py
@@ -0,0 +1,26 @@
+#while loop
+# count=1
+# while count<=5:
+#     print(count)
+#     count+=1
+
+# count=10
+# while count>=1:
+#     print(count)
+#     count-=1
+
+# count=1
+# while count<=10:
+#     if count%2==0:
+#         print(count)
+#     count+=1
+
+sum=0
+n=int(input("enter a number: "))
+while n!=-1:
+    sum=sum+n
+    n=int(input("enter a number: "))
+print(sum)
+
+    
+    
